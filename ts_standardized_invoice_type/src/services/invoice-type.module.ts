a7a3f45e50b3b8058da366e9b0bf8cd4780252e8
Indy <i.vancanegem@metamaze.eu>
2024-01-10 18:08:05 +0100

    chore: update docs

--- ts_standardized_invoice_type/src/services/invoice-type.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { InvoiceTypeService } from './invoice-type.service';
-
-@Module({
-  providers: [InvoiceTypeService]
-})
-export class InvoiceTypeModule {}
